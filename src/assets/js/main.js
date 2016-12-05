b0297071099e885314bdc64c3fd026c462bdf564
Yate <jiesa2046@126.com>
2016-12-05 22:26:27 +0800

    update index

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,39 @@
 require("bootstrap");
 
-$(function (cb) {
-   _.each([1, 2, 3], function (e) {
-      console.log(e);
+(function ($, undefined) {
+   "use strict";
+
+   var $document = $(document);
+   $document.ready(function () {
+      $(".scroll-down").arctic_scroll();
    });
-});
+
+   $.fn.arctic_scroll = function (options) {
+      var defaults = {
+             elem: $(this),
+             speed: 500
+          },
+
+          allOptions = $.extend(defaults, options);
+
+      allOptions.elem.click(function (event) {
+         event.preventDefault();
+         var $this = $(this),
+             $htmlBody = $('html, body'),
+             offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
+             position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
+             toMove;
+
+         if (offset) {
+            toMove = parseInt(offset);
+            $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
+         } else if (position) {
+            toMove = parseInt(position);
+            $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
+         } else {
+            $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
+         }
+      });
+
+   };
+})(jQuery);
